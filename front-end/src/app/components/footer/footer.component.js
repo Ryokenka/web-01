0ea03252f8ae42ec3267591648b348bda4d1b879
Ryokenka <remi.guerin99@gmail.com>
2024-03-06 15:44:01 +0100

    Switched to a Component Oriented Architecture

---
+++ front-end/src/app/components/footer/footer.component.js
@@ -0,0 +1,9 @@
+import "./footer.component.css";
+import template from "./footer.component.html";
+
+export class FooterComponent extends HTMLElement {
+    constructor() {
+        super();
+        this.innerHTML = template;
+    }
+}
---
+++ front-end/src/app/components/navbar/navbar.component.js
@@ -0,0 +1,9 @@
+import "./navbar.component.css";
+import template from "./navbar.component.html";
+
+export class NavbarComponent extends HTMLElement {
+    constructor() {
+        super();
+        this.innerHTML = template;
+    }
+}
