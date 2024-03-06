1162d1d2176fe9dd95f5c80472c9a3009d0a2051
Ryokenka <remi.guerin99@gmail.com>
2024-03-06 13:49:19 +0100

    Asynchronous

--- front-end/src/app/scripts/game.js
+++ front-end/src/app/scripts/game.js
@@ -30,11 +30,9 @@ export class GameComponent extends Component {
   }
 
   /* method GameComponent.init */
-  init() {
-    // fetch the cards configuration from the server
-    this.fetchConfig((config) => {
-      this._config = config;
-      this._boardElement = document.querySelector(".cards");
+  async init() {
+    this._config = await this.fetchConfig();
+    this._boardElement = document.querySelector(".cards");
 
       // create cards out of the config
       this._cards = this._config.ids.map((id) => new CardComponent(id));
@@ -48,8 +46,7 @@ export class GameComponent extends Component {
       });
 
       this.start();
-    });
-  }
+    }
 
   /* method GameComponent.start */
   start() {
@@ -67,30 +64,11 @@ export class GameComponent extends Component {
   }
 
   /* method GameComponent.fetchConfig */
-  fetchConfig(cb) {
-    let xhr =
-      typeof XMLHttpRequest != "undefined"
-        ? new XMLHttpRequest()
-        : new ActiveXObject("Microsoft.XMLHTTP");
-
-    xhr.open("get", `${environment.api.host}/board?size=${this._size}`, true);
-
-    xhr.onreadystatechange = () => {
-      let status;
-      let data;
-      // https://xhr.spec.whatwg.org/#dom-xmlhttprequest-readystate
-      if (xhr.readyState == 4) {
-        // `DONE`
-        status = xhr.status;
-        if (status == 200) {
-          data = JSON.parse(xhr.responseText);
-          cb(data);
-        } else {
-          throw new Error(status);
-        }
-      }
-    };
-    xhr.send();
+  async fetchConfig() {
+    const response = await fetch(
+        `${environment.api.host}/board?size=${this._size}`
+    );
+    return response.json();
   }
 
   /* method GameComponent.goToScore */
